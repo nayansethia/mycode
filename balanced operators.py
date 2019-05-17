fdcfe4347cc572fe73115a113c9e9dbec257445b
Nayan Sethia <sethianayan15@gmail.com>
2019-05-18 01:30:56 +0530

    The first commit for balanced.py

---
+++ balanced operators.py
@@ -0,0 +1,34 @@
+from stack import stack
+s=stack()
+l=input()
+t=1
+for i in range(len(l)):
+    if l[i] =="{" or l[i]=="[" or l[i]=="(":
+        s.push(l[i])
+    elif s.isempty():
+        print("Not balanced")
+        t=0
+        break
+    elif l[i]=="}":
+        k=s.pop()
+        if k!="{":
+            print("Not balanced")
+            t=0
+            break
+    elif l[i]=="]":
+        k=s.pop()
+        #print(k)
+        if k!="[":
+            print("Not balanced")
+            t=0
+            break
+    elif l[i]==")":
+        k=s.pop()
+        if k!="(":
+            print("Not balanced")
+            t=0
+            break
+    #s.printstack()
+if t==1:
+    print("Balanced")
+    
